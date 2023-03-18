ee6bedd124075bd73c713443808ae0b41ac376f3
Niyaj-Kumanali <iamniyazahmad777@gmail.com>
2023-03-18 14:14:44 +0530

    java

--- DeleteConsonantFromString.java
+++
@@ -1,8 +0,0 @@
-public class DeleteConsonantFromString {
-    public static void main(String[] args) {
-        String str = "Hello, have a good day";
-        String consonants = "([BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz]+)";
-        String result = str.replaceAll(consonants,"");
-        System.out.println(result);
-    }
-}
--- printAlphabet.java
+++
@@ -1,7 +0,0 @@
-public class printAlphabet {
-    public static void main(String[] args) {
-        for(int i = 65; i<91; i++){
-            System.out.print((char)i + " ");
-        }
-    }
-}
